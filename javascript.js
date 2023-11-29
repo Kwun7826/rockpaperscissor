4a46e70700fec82e9b1c65621695583fc5bd2f77
Kwun7826 <nguyenthanhlong7826@gmail.com>
2023-11-29 13:20:27 +0700

    Play one round of rock-paper-scissors

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
 function getComputerChoice(){
-    let random = Math.floor(Math.number() * 3 );
-        if (random = 0){
-            return 'ROCK';
-        } else if (random = 1){
-            return 'PAPER';
+    let random = Math.floor(Math.random() * 3 );
+        if (random === 0){
+            return "ROCK";
+        } else if (random === 1){
+            return "PAPER";
         } else {
             return "SCISSORS";
         }
-}
+    }        
 
+function playRound(playerSelection, computerSelection){
+  if (playerSelection === "ROCK" && computerSelection === "ROCK"){
+    return "You TIE!";
+  } else if (playerSelection === "ROCK" && computerSelection === "PAPER"){
+    return "You LOSE!";
+  } else {
+    return "You WON!";
+  }
+}
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
