33cdb189ea97bcccd7ab97d247bc9ea1f0dc4e33
Kwun7826 <nguyenthanhlong7826@gmail.com>
2023-11-29 12:18:16 +0700

    Get random result

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+    let random = Math.floor(Math.number() * 3 );
+        if (random = 0){
+            return 'ROCK';
+        } else if (random = 1){
+            return 'PAPER';
+        } else {
+            return "SCISSORS";
+        }
+}
+
